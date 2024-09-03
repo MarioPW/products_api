fffd64ddc9635a61d5e2ab8db8d6ada8c3cec9c5
Mario Triana <mariotriana1978@gmail.com>
2024-09-03 17:03:54 -0500

    adding dev host and port

--- main.py
+++ main.py
@@ -37,5 +37,5 @@ def docs_layout(request: Request):
 if __name__ == "__main__":
     import uvicorn
     host = getenv('HOST', '127.0.0.1')
-    port = getenv('PORT', 8000)
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(getenv("PORT", 8000))
+    uvicorn.run(app, host="host", port=port)
